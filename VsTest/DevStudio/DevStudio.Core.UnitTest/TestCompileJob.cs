f282164f0fbdac17659ee28caa21eac1ec916750
Yi Zeng <yize@microsoft.com>
2019-07-05 15:21:10 +0800

    repro project

---
+++ VsTest/DevStudio/DevStudio.Core.UnitTest/TestCompileJob.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace DevStudio.Core.UnitTest
+{
+    [TestClass]
+    public class TestCompileJob
+    {
+        [TestMethod]
+        public void TestRestore()
+        {
+            string args = $@"restore Compiler.csproj";
+            Process restoreProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo("dotnet", args)
+            };
+            restoreProcess.Start();
+            restoreProcess.WaitForExit();Assert.AreEqual(0, restoreProcess.ExitCode);
+        }
+    }
+}
